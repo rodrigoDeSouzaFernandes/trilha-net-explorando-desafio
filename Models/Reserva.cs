a8593aaf407550256a45f04d4a58f69c7c8a81da
Rodrigo <rdesouza2008@gmaill.com>
2025-05-19 10:52:01 -0300

    completa as funções do desafio básico em C#

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,9 +15,11 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
+            int quantidadeHospedes = hospedes.Count();
+
             // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= quantidadeHospedes)
             {
                 Hospedes = hospedes;
             }
@@ -25,6 +27,9 @@ namespace DesafioProjetoHospedagem.Models
             {
                 // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
                 // *IMPLEMENTE AQUI*
+                throw new Exception(
+                    $"Capacidade insuficiente, esta suíte acomoda no máximo {Suite.Capacidade} hóspede(s)"
+                );
             }
         }
 
@@ -37,7 +42,7 @@ namespace DesafioProjetoHospedagem.Models
         {
             // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
             // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count();
         }
 
         public decimal CalcularValorDiaria()
@@ -45,13 +50,13 @@ namespace DesafioProjetoHospedagem.Models
             // TODO: Retorna o valor da diária
             // Cálculo: DiasReservados X Suite.ValorDiaria
             // *IMPLEMENTE AQUI*
-            decimal valor = 0;
+            decimal valor = DiasReservados * Suite.ValorDiaria;
 
             // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
             // *IMPLEMENTE AQUI*
-            if (true)
+            if (DiasReservados >= 10)
             {
-                valor = 0;
+                return valor * 0.9M;
             }
 
             return valor;
